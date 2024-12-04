53d21d34b0d054bcf09ea83d341f176549fe1584
Erik Riquelme <erikone2311@gmail.com>
2024-12-04 22:50:54 +0100

    Apply cors middleware when the enviroment is not test

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import gamesRouter from "../game/router/gamesRouter.js";
 import handleEndpointNotFound from "./errors/handleEndpointNotFound.js";
 import handleGeneralErrors from "./errors/handleGeneralErrors.js";
@@ -8,6 +9,21 @@ const app = express();
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+
+if (process.env.NODE_ENV !== "test") {
+  const url = process.env.WHITELIST_URLS;
+  if (!url) {
+    throw new Error("Envaironment variable WHITELIST_URLS does not exist");
+  }
+
+  const urls = url.split(",");
+
+  app.use(
+    cors({
+      origin: urls,
+    }),
+  );
+}
 
 app.use("/games", gamesRouter);
 
