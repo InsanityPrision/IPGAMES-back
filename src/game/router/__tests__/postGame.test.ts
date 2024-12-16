40644f4af5419a2dd20c4ebc0f2a328cc76c7926
Erik Riquelme <erikone2311@gmail.com>
2024-12-16 13:26:01 +0100

    Change date type from Date to string

--- src/game/router/__tests__/postGame.test.ts
+++ src/game/router/__tests__/postGame.test.ts
@@ -15,7 +15,7 @@ describe("Given the POST /games endpoint", () => {
     description:
       "Outer Wilds is an open-world mystery game where players explore a solar system trapped in a 22-minute time loop. As a Hearthian astronaut, you uncover the secrets of the Nomai civilization and the Eye of the Universe. The dynamic planets evolve in real time, creating a unique puzzle-solving experience. Praised for its storytelling and innovative design, it’s a standout in exploration games​",
     developer: "Mobius Digital",
-    date: new Date("2020-5-18"),
+    date: "2020-5-18",
     genders: ["Horror", "Adventure"],
     imageUrl: "/outerwilds.webp",
     imageAlt: "Outer Wilds cover",
