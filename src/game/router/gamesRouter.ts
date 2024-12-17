9c3b0092749f93b79a5a07eed741cdccd4caf5da
Erik Riquelme <erikone2311@gmail.com>
2024-12-17 23:24:32 +0100

    Create new endpoint GET /games:id

--- src/game/router/gamesRouter.ts
+++ src/game/router/gamesRouter.ts
@@ -8,5 +8,6 @@ const gamesController = new GamesController(GameModel);
 gamesRouter.get("/", gamesController.get);
 gamesRouter.post("/", gamesController.post);
 gamesRouter.delete("/:_id", gamesController.delete);
+gamesRouter.get("/:_id", gamesController.getById);
 
 export default gamesRouter;
